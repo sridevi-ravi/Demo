0f98619e7c66765213906afa6899742e3bf1e613
sridevi-ravi <sridevi.b.ravi@accenture.com>
2023-10-19 11:01:46 +0000

    Week 1

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
+	"use strict";
+
+	return Controller.extend("demo.sapui5.controller.View1", {
+		onInit: function () {
+
+		},
+		onClick: function () {
+			MessageToast.show("Button Pressed");
+		}
+	});
+});
